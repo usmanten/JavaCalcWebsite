a434b037876ddc5a7b782e67843d6f7b620ac64b
usmanten <149915881+usmanten@users.noreply.github.com>
2025-04-20 19:07:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function add() {
-    var num1 = Number(document.form1.txtNum1.value);
-    var num2 = Number(document.form1.txtNum2.value);
+    const num1 = Number(document.form1.txtNum1.value);
+    const num2 = Number(document.form1.txtNum2.value);
 
     if (isNaN(num1) || isNaN(num2)) {
         alert("Please enter valid numbers in both fields.");
@@ -8,4 +8,16 @@ function add() {
     }
 
     document.form1.txtSum.value = num1 + num2;
+}
+
+function subtract() {
+    const num1 = Number(document.form1.txtNum1.value);
+    const num2 = Number(document.form1.txtNum2.value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Please enter valid numbers in both fields.");
+        return;
+    }
+
+    document.form1.txtSum.value = num1 - num2;
 }
