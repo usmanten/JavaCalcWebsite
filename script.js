ac05b8b87f124ef0c64ff6ecb94c0d5bb3d63066
usmanten <149915881+usmanten@users.noreply.github.com>
2025-04-20 19:29:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function add() {
 
 function subtract() {
     const num1 = Number(document.form1.txtNum1.value);
-    const num2 = Number(form1.txtNum2.value);
+    const num2 = Number(document.form1.txtNum2.value);
 
     if (isNaN(num1) || isNaN(num2)) {
         alert("Please enter valid numbers in both fields.");
